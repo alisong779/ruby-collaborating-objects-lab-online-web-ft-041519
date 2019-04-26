075a07f1b0359955202fa27c3c4d59c0e8599a6d
Alison Golden <alisong779@yahoo.com>
2019-04-26 21:28:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,8 +7,8 @@ class Song
   end 
 
   def self.new_by_filename(filename)
-    song_name = file_name.split(" - ")[1]
-    artist_name = file_name.split(" - ")[0]
+    song_name = filename.split(" - ")[1]
+    artist_name = filename.split(" - ")[0]
     new_song = self.new(song_name)
     new_song.artist_name = artist 
     new_song 

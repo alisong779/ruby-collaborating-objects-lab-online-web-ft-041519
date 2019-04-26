6529939a9f2a6b2a417956574cd1fc8ad2d07f8a
Alison Golden <alisong779@yahoo.com>
2019-04-26 21:10:35 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,8 +7,7 @@ class MP3Importer
   end 
   
   def files
-    #files = []
-    #Dir.entries(path).select {entry.include?("mp3")}
+    Dir.entries(path).select {entry.include?("mp3")}
   end 
   
    def import(list_of_filenames)

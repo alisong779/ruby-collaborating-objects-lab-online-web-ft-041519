9b539d49b58661b91de7cd1fada974f4256f5158
Alison Golden <alisong779@yahoo.com>
2019-04-26 21:41:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,8 +14,8 @@ class Song
     new_song 
   end 
   
-  def artist_name(name)  
-    self.artist = Artist.find_or_create_by_name(name)
+  def artist_name 
+    self.artist = Artist.find_or_create_by_name
   end 
   
 end 
